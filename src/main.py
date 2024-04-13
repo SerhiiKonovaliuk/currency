255ab982cd71fe57a149e27fabb06389a948d100
SerhiiKonovaliuk <skonovaluk5@gmail.com>
2024-04-13 15:33:44 +0300

    Add templates

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Привіт світ з Академії Професій Майбутнього")
+from flask import *
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    nums = [1,2,3,4,5,6]
+    return render_template("index.html", nums=nums)
+
+if __name__ == '__main__':
+    app.run()
