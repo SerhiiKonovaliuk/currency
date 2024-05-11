329aee9cac71fe7de76e80cc31d8bfdbc189977a
SerhiiKonovaliuk <skonovaluk5@gmail.com>
2024-05-11 15:54:43 +0300

    Add converter to template

--- src/converter_form.py
+++ src/converter_form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, FloatField, SelectField
 from wtforms.validators import DataRequired, Length, Regexp
 
 class ConverterForm(FlaskForm):
-    amount = FloatField('Amount', validators=[DataRequired(), Regexp(r"\d*")])
-    fromCurrency = SelectField("From", choices=["USD", "UAH", "EUR"])
-    toCurrency = SelectField("To",choices=["USD", "UAH", "EUR"])
+    amount = FloatField('Сума', validators=[DataRequired(), Regexp(r"\d*")])
+    fromCurrency = SelectField("З", choices=["USD", "UAH", "EUR"])
+    toCurrency = SelectField("До",choices=["USD", "UAH", "EUR"])
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from flask import *
 from py_currency_converter import convert
+from converter_form import ConverterForm
+from flask_wtf.csrf import CSRFProtect
+import os
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def index():
@@ -10,7 +16,7 @@ def index():
         "EUR" : convert(base="EUR", amount=1, to=['UAH'])["UAH"],
         "PLN" : convert(base="PLN", amount=1, to=['UAH'])["UAH"]
     }
-    return render_template("index.html.j2", currencies = currencies)
+    return render_template("index.html.j2", currencies = currencies, form = ConverterForm())
 
 if __name__ == '__main__':
     app.run()
