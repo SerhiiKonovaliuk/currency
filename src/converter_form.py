535c3e8d1598987747f42193dfd7c7c9601bc763
SerhiiKonovaliuk <skonovaluk5@gmail.com>
2024-05-25 15:32:51 +0300

    Add converter form

--- src/converter_form.py
+++ src/converter_form.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, SelectField
+from flask_wtf.form import _Auto
+from wtforms import StringField, FloatField, SelectField, SubmitField, Label
 from wtforms.validators import DataRequired, Length, Regexp
+from py_currency_converter.py_exchange_rate import country_codes
 
 class ConverterForm(FlaskForm):
     amount = FloatField('Сума', validators=[DataRequired(), Regexp(r"\d*")])
-    fromCurrency = SelectField("З", choices=["USD", "UAH", "EUR"])
-    toCurrency = SelectField("До",choices=["USD", "UAH", "EUR"])
+    fromCurrency = SelectField("З  ", choices=country_codes)
+    toCurrency = SelectField("До",choices=country_codes)
+    result = Label("res", "Результат: ")
+    submit = SubmitField("Конвертувати")
 
--- src/main.py
+++ src/main.py
@@ -9,14 +9,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect(app)
 
-@app.route("/")
-def index():
+@app.route("/", methods=['GET', 'POST'])
+def get_index():
     currencies = {
         "USD" : convert(base="USD", amount=1, to=['UAH'])["UAH"],
         "EUR" : convert(base="EUR", amount=1, to=['UAH'])["UAH"],
         "PLN" : convert(base="PLN", amount=1, to=['UAH'])["UAH"]
     }
-    return render_template("index.html.j2", currencies = currencies, form = ConverterForm())
-
+    form = ConverterForm()
+    base = request.form.get("fromCurrency")
+    amount = request.form.get("amount")
+    to = request.form.get("toCurrency")
+    if base != None and amount != None and to != None:
+        converted = convert(base=base, amount=int(amount), to=[to])[to]
+        form.result = "{:.2f}".format(converted)
+    return render_template("index.html.j2", currencies = currencies, form = form)
 if __name__ == '__main__':
     app.run()
